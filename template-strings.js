2fb1125af3c405f1a42024806cbea5a961bba95b
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2022-10-16 17:43:41 -0300

    modificações em operadores e funções

--- template-strings.js
+++ template-strings.js
@@ -1,10 +1,10 @@
-const nome = "Ju";
-const idade = 2021-1981;
-const cidadeDeNascimento = "São Paulo";
+const nome = "Wollacy";
+const idade = 2022-1990;
+const cidadeDeNascimento = "Joinville";
 
 // const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
 
-const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} anos e nasci na cidade de ${cidadeDeNascimento}`;
 
 console.log(apresentacao)
 
