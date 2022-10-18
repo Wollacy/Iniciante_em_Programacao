d1419bc69a2ae229f3a523817e8e88a5147d52e7
Wollacy Willyan de Carvalho <47800346+Wollacy@users.noreply.github.com>
2022-10-18 05:39:11 -0300

    Add files via upload

---
+++ Javascript Back-end/template-strings.js
@@ -0,0 +1,11 @@
+const nome = "Wollacy";
+const idade = 2022-1990;
+const cidadeDeNascimento = "Joinville";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} anos e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
+
+
---
+++ Javascript Back-end/tiposVariaveis.js
@@ -0,0 +1,22 @@
+// Tipo - Number
+
+const num1 = 1;
+const num2 = 2;
+
+console.log(num1 + num2);
+
+// Tipo - String
+
+const texto = 'Olá Mundo!';
+const meuNome = 'Wollacy'
+
+console.log(texto + ' ' + meuNome);
+
+// Tipo - Boolean
+
+const primeiroNum = 5;
+const segundoNum = 10;
+const terceiroNum = 5;
+
+console.log(primeiroNum === segundoNum);
+console.log(primeiroNum === terceiroNum);
